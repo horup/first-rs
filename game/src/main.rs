c9bd5a1bad04de20443dcff9999e10462712e9ea
Søren Alsbjerg Hørup <sah@sah.dk>
2023-01-03 20:30:31 +0100

    played with rebuild times

--- game/src/main.rs
+++ game/src/main.rs
@@ -7,14 +7,13 @@ impl Game for MyGame {
     fn update(&mut self, ctx:&mut engine::Context) {
         ctx.draw();
     }
-}
+} 
 
 fn main() {
     pollster::block_on(async {
         let g = MyGame {
-
         };
         let engine = Engine::new(g).await;
         engine.run().await; 
-    });
-}
+    }); 
+} 
