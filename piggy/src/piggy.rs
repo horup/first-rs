f01847f424f2daadf392e96bc70a9303ca27766b
Søren Alsbjerg Hørup <sah@sah.dk>
2023-01-27 10:36:24 +0100

    added crosshair

--- piggy/src/piggy.rs
+++ piggy/src/piggy.rs
@@ -1,4 +1,4 @@
-use engine_sdk::{Game, image, Event, Map, Engine, DrawTextParams, glam::vec2, Color};
+use engine_sdk::{Game, image, Event, Map, Engine, DrawTextParams, glam::vec2, Color, DrawLineParams};
 use serde::{Serialize, Deserialize};
 
 #[derive(Default, Serialize, Deserialize)]
@@ -8,11 +8,29 @@ pub struct Piggy {
 
 impl Piggy {
     pub fn update_ui(&mut self, engine:&mut dyn Engine) {
+        let margin = vec2(16.0, 16.0);
+        let center = engine.screen_size() / 2.0;
         engine.draw_text(DrawTextParams {
-            screen_pos: vec2(0.0, 0.0),
+            screen_pos: margin + vec2(0.0, 0.0),
             text: "Hello world".into(),
             scale: 16.0,
             color: Color::WHITE,
+        });
+
+        let l = 8.0;
+        let w = 1.0;
+        engine.draw_line(DrawLineParams {
+            begin: center + vec2(-l,0.0),
+            end: center + vec2(l,0.0),
+            line_width: w,
+            color: Color::WHITE
+        });
+
+        engine.draw_line(DrawLineParams {
+            begin: center + vec2(0.0,-l),
+            end: center + vec2(0.0,l),
+            line_width: w,
+            color: Color::WHITE
         })
     }
 }
