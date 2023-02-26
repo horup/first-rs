31fbff16c5bc56d48840d61eeba536d30c80d939
= <horup@deepstacker.com>
2023-02-26 08:16:06 +0100

    updated

--- engine_sdk/src/spatial_hashmap.rs
+++ engine_sdk/src/spatial_hashmap.rs
@@ -8,35 +8,47 @@ use flat_spatial::{Grid as FlatGrid, grid::GridHandle};
 
 pub struct SpatialHashmap {
     grid:FlatGrid<SpriteId, [f32;2]>,
-    handles:SlotMap<SpriteId, GridHandle>
+    handles:SlotMap<SpriteId, GridHandle>,
+    max_radius:f32
 }
 
 impl SpatialHashmap {
-    pub fn max_radius(&self) -> f32 {
-        1.0
-    }
+   
     pub fn new(sprites:&Entities<SpriteId, Sprite>) -> Self {
         let cell_size = 8;
         let grid = FlatGrid::new(cell_size);
         let mut spatial = Self {
             grid,
-            handles:SlotMap::default()
+            handles:SlotMap::default(),
+            max_radius:1.0
         };
 
         for (sprite_id, sprite) in sprites.iter() {
-            spatial.update(sprite_id, sprite.pos.truncate());
+            spatial.update_pos(sprite_id, sprite.pos.truncate());
+            spatial.max_radius = if spatial.max_radius < sprite.radius { sprite.radius } else { spatial.max_radius };
         }
 
         spatial
     }
 
-    pub fn update(&mut self, id:SpriteId, pos:Vec2) {
+    pub fn update_pos(&mut self, id:SpriteId, pos:Vec2) {
         if let Some(handle) = self.handles.get(id) {
             self.grid.remove_maintain(*handle);
         }
 
         let key = self.grid.insert([pos.x, pos.y], id);
         self.handles.insert(key);
+    }
+
+    pub fn query(&self, sprite:(SpriteId, &Sprite), results:&mut Vec<SpriteId>) {
+        results.clear();
+        for (handle, _) in self.grid.query_around([sprite.1.pos.x, sprite.1.pos.y], sprite.1.radius + self.max_radius) {
+            if let Some((_, sprite_id)) = self.grid.get(handle) {
+                if sprite.0 != *sprite_id {
+                    results.push(sprite_id.clone());
+                }
+            }
+        }
     }
 
     pub fn query_around(&self, pos:Vec2, radius:f32, results:&mut Vec<SpriteId>) {
@@ -75,7 +87,7 @@ fn test() {
     for (id, sprite) in sprites.iter_mut() {
         sprite.pos.x = size as f32 / 2.0;
         sprite.pos.y = size as f32 / 2.0;
-        spatial.update(id, sprite.pos.truncate());
+        spatial.update_pos(id, sprite.pos.truncate());
     }
     let elapsed = Instant::now() - now;
     println!("updating all sprite positions took {}ms", elapsed.as_millis());
