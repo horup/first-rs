5888d45d43413c5bfe08fb682156f4b39d8b769a
= <horup@deepstacker.com>
2023-03-08 21:21:50 +0100

    replaced item_System with thing abstraction

--- piggy/src/systems/activator_system.rs
+++ piggy/src/systems/activator_system.rs
@@ -2,44 +2,6 @@ use engine_sdk::Engine;
 use crate::State;
 
 pub fn activator_system(state:&mut State, engine:&mut dyn Engine) {
-    // check proximity to door
-   /*  if let Some(player_id) = state.player_id {
-        if let Some(player_sprite) = state.sprites.get(player_id) {
-            
-            let player_pos = player_sprite.pos;
-            let mut near = Vec::new();
-            let mut world = state.as_world();
-            let radius = 1.0;
-            world.query_around(player_pos.truncate(), radius, &mut near);
-            for id in near.drain(..) {
-                if let Some(activator) = state.activators.get_mut(id) {
-                    match activator {
-                        crate::components::Activator::Door { key } => {
-                            let can_open = match key {
-                                Some(key) => {
-                                    let mut can_open = false;
-                                    if let Some(player) = state.players.get(player_id) {
-                                        if player.inventory.has(*key) {
-                                            can_open = true;
-                                        }
-                                    } 
-
-                                    can_open
-                                },
-                                None => true,
-                            };
-                            if can_open {
-                                if let Some(door) = state.doors.get_mut(id) {
-                                    door.open();
-                                }
-                            }
-                        },
-                    }
-                }
-            }
-        }
-    }*/
-
     for (id, _) in state.sprites.iter() {
         if let Some(activatee_thing) = state.activatee_thing(id) {
             let player_pos = activatee_thing.sprite.pos;
--- piggy/src/systems/item_system.rs
+++ piggy/src/systems/item_system.rs
@@ -2,21 +2,19 @@ use engine_sdk::Engine;
 use crate::{State, textures};
 
 pub fn item_system(state:&mut State, _engine:&mut dyn Engine) {
-    if let Some(player_id) = state.player_id {
-        if let Some(player) = state.sprites.get(player_id) {
-            let player_pos = player.pos;
-            let pickup_radius = 0.5;
-            let mut world = state.as_world();
-            let mut near = Vec::new();
-            world.query_around(player_pos.truncate(), pickup_radius, &mut near);
-            for id in near.drain(..) {
-                if let (Some(sprite), Some(item)) = (state.sprites.get(id), state.items.get(id)) {
-                    let texture = sprite.texture;
-                    state.sprites.despawn(id);
-                    state.flash.flash(0.2, 0.5);
-                    if let Some(player) = state.players.get_mut(player_id) {
-                        player.inventory.add(texture, 1.0);
-                    }
+    if let Some(player_thing) = state.player_thing() {
+        let player_pos = player_thing.sprite.pos;
+        let pickup_radius = 0.5;
+        let mut world = state.as_world();
+        let mut near = Vec::new();
+        world.query_around(player_pos.truncate(), pickup_radius, &mut near);
+        for id in near.drain(..) {
+            if let Some(item_thing) = state.item_thing(id) {
+                let texture = item_thing.sprite.texture;
+                state.sprites.despawn(id);
+                state.flash.flash(0.2, 0.5);
+                if let Some(player_thing) = state.player_thing() {
+                    player_thing.player.inventory.add(texture, 1.0);
                 }
             }
         }
