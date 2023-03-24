894ff3a822bcd6a270f551a3e2b6f748155ff5c8
= <horup@deepstacker.com>
2023-03-24 18:58:11 +0100

    wip

--- engine_sdk/src/engine.rs
+++ engine_sdk/src/engine.rs
@@ -1,8 +1,18 @@
-use glam::Vec2;
+use glam::{Vec2, IVec2};
 use image::DynamicImage;
+use serde::{Serialize, Deserialize};
 use winit::{event::VirtualKeyCode};
+use world::EntityId;
 use crate::world::World;
-use crate::{Camera, Color, Event, Atlas, TextureAtlas, EditorProps};
+use crate::{Camera, Color, Event, Atlas, TextureAtlas, EditorProps, SpatialHashmap, Sprite};
+
+#[derive(Default, Serialize, Deserialize, Clone, Copy)]
+pub struct Collision {
+    pub entity:EntityId,
+    pub other_entity:Option<EntityId>,
+    pub tile:Option<IVec2>
+}
+
 
 pub trait Engine {
     fn egui(&self) -> &egui::Context;
@@ -28,6 +38,116 @@ pub trait Engine {
     }
     fn set_cursor_grabbed(&mut self, grabbed:bool);
     fn cursor_grabbed(&self) -> bool;
+
+    fn physics_step(&mut self, world:&World, collisions:&mut Vec<Collision>)  {
+        let dt = self.dt();
+        collisions.clear();
+        let spatial_hashmap = SpatialHashmap::new(world);
+        
+        for e in world.entities() {
+            if let Some(sprite) = e.get_mut::<Sprite>() {
+                let new_pos = sprite.pos + sprite.vel * dt;
+                let collision = self.clip_move(e.id(), new_pos, &mut spatial_hashmap);
+                if collision.other_entity.is_some() || collision.tile.is_some() {
+                    collisions.push(collision);
+                }
+            }
+        }
+    }
+
+    fn clip_move(&mut self, id:EntityId, new_pos:Vec3, spatial_hashmap:&mut SpatialHashmap) -> Collision {
+        let mut col = Collision::default();
+        col.entity = id;
+        if let Some(e) = self.sprites.get_mut2(id) {
+            let v = new_pos - e.pos;
+            if v.length() > 0.0 {
+                let mut left = v.length();
+                let d = v.normalize();
+
+                // FIXME: max step should be configurable at some point
+                let max_step = 1.0 / 16.0;
+                const DIMS: [Vec2; 2] = [Vec2::new(0.0, 1.0), Vec2::new(1.0, 0.0)];
+                while left > 0.0 {
+                    let mut step = left;
+                    if step > max_step {
+                        step = max_step;
+                    }
+                    let v = d * step;
+                    left -= step;
+
+                    for dim in DIMS {
+                        let pos_org = e.pos;
+                        let v = v.truncate() * dim;
+                        if v.length() == 0.0 {
+                            continue;
+                        }
+
+                        let mut pos_new = pos_org + v.extend(0.0);
+
+                        // collision handling between entities
+                        self.spatial_hashmap.query_around(e.pos.truncate(), e.radius + v.length() + self.spatial_hashmap.max_radius(), &mut self.potential_colliders);
+                        for other_id in self.potential_colliders.iter() {
+                            let other_e = self.sprites.get(*other_id).unwrap();
+                            let ignore = !e.clips || !other_e.clips;
+                            if *other_id != id && !ignore {
+                                let s1_pos = Isometry2::translation(pos_new.x, pos_new.y);
+                                let s1 = parry2d::shape::Ball::new(e.radius);
+                                let aabb1 = s1.aabb(&s1_pos);
+                                let s2_pos = Isometry2::translation(other_e.pos.x, other_e.pos.y);
+                                let s2 = parry2d::shape::Ball::new(other_e.radius);
+                                let aabb2 = s2.aabb(&s2_pos);
+
+                                if aabb1.intersects(&aabb2) {
+                                    pos_new = pos_org;
+                                    
+                                    // FIXME: last collision is saved, even though multiple might exist
+                                    col.other_entity = Some(*other_id);
+                                }
+                            }
+                        }
+
+                        // collision between grid
+                        let v = pos_new - pos_org;
+                        let v = v.truncate() * dim;
+                        let d = v.normalize();
+                        let rev_dim = Vec2::new(dim.y, dim.x);
+                        for i in [-1, 0, 1] {
+                            let i = i as f32;
+                            let cp = Vec2::new(i, i) * rev_dim + d + pos_org.truncate();
+                            let np = cp.as_ivec2();
+                            if let Some(cell) = self.tilemap.get((np.x, np.y)) {
+                                if cell.clips {
+                                    let s1 =
+                                        parry2d::shape::Cuboid::new([e.radius, e.radius].into());
+                                    let s1_pos = Isometry2::translation(pos_new.x, pos_new.y);
+                                    let aabb1 = s1.aabb(&s1_pos);
+                                    let s2 = parry2d::shape::Cuboid::new([0.5, 0.5].into());
+                                    let s2_pos = Isometry2::translation(
+                                        np.x as f32 + 0.5,
+                                        np.y as f32 + 0.5,
+                                    );
+                                    let aabb2 = s2.aabb(&s2_pos);
+
+                                    if aabb1.intersects(&aabb2) {
+                                        pos_new = pos_org;
+
+                                        col.tile = Some(np);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+
+                        e.pos = pos_new;
+                        self.spatial_hashmap.update_one(id, e.pos.truncate());
+                    }
+                }
+            }
+        }
+        col
+    }
+
+
 }
 
 
--- piggy/src/systems/physics_system.rs
+++ piggy/src/systems/physics_system.rs
@@ -2,11 +2,8 @@ use std::mem::take;
 
 use engine_sdk::Engine;
 
-use crate::State;
-
-pub fn physics_system(state:&mut State, engine:&mut dyn Engine) {
+pub fn physics_system(world:&mut World, engine:&mut dyn Engine) {
     let mut collisions = take(&mut state.collisions);
-    let mut world = state.as_world();
-    world.physics_step(engine.dt(), &mut collisions);
-    state.collisions = collisions;
+    //world.physics_step(engine.dt(), &mut collisions);
+    //state.collisions = collisions;
 }
