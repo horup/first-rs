47ebd2353c0c5575733f91bc54a9412ef9dc0f9a
= <horup@deepstacker.com>
2023-03-24 15:52:40 +0100

    wip

--- piggy/src/systems/render_world_system.rs
+++ piggy/src/systems/render_world_system.rs
@@ -1,11 +1,10 @@
 use engine_sdk::Engine;
-use crate::State;
 
-pub fn render_world_system(state:&mut State, engine:&mut dyn Engine) {
+pub fn render_world_system(world:&mut World, engine:&mut dyn Engine) {
     let cam = state.camera;
     // draw scene
     engine.draw_scene(
         &cam,
-        &mut state.as_world(),
+        world,
     );
 }
